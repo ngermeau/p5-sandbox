d1d3d26909f7353042d34cce4fd8600f1f4c8801
Nicolas Germeau <ngermeau@icloud.com>
2023-02-09 22:26:36 +0100

    basic animation

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 function setup() {
   createCanvas(2000, 2000)
 }
+let p = 200
+
+function shape(where) {
+  let ran = Math.random() * (100 - 3) + 3
+  ellipse(where, p + ran, 50 + ran, 50 + ran)
+  ellipse(where, p - ran, 30 - ran, 90 - ran)
+  p += 0.4
+
+}
 
 function draw() {
   background("#151515")
-  for (let i = 0; i < 900; i += 5) {
-    ellipse(i, i, i, i)
-  }
+  shape(700)
+  shape(500)
+  shape(400)
+  shape(300)
+  shape(200)
 }
