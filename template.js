68ae1c29c3db52fe2aa223b5272792815a1a4dbe
Nicolas Germeau <ngermeau@icloud.com>
2023-12-07 19:54:05 +0100

    glife

---
+++ template.js
@@ -0,0 +1,20 @@
+
+let config = {
+  backgroundColor: 22,
+}
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(config.backgroundColor)
+  square(10, 10, 50, 50);
+}
+
